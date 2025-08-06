103d493de35649b66af196148f9e65ae9f815cb0
RonRotich <kiprotichronald@gmail.com>
2025-08-06 14:15:03 +0300

    Added week 2 assignement

---
+++ assignmentwk2.py
@@ -0,0 +1,19 @@
+
+Ronlist = []
+
+Ronlist.append(10)
+Ronlist.append(20)
+Ronlist.append(30)
+Ronlist.append(40)
+
+Ronlist.insert(1, 15)
+
+Ronlist.extend([50, 60, 70])
+
+Ronlist.pop()
+
+Ronlist.sort()
+
+index_of_30 = Ronlist.index(30)
+print(Ronlist)
+print("Index of 30:", index_of_30)
